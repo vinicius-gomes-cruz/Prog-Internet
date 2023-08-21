290504baff8be50b9bdbb75799ced47a818e14d0
vinicius-gomes-cruz <124798795+vinicius-gomes-cruz@users.noreply.github.com>
2023-08-21 09:59:24 -0300

    Add files via upload

--- API/API.js
+++ API/API.js
@@ -5,8 +5,22 @@ async function API() {
     console.log(dados);
     const musicas = await dados.json();
     console.log(musicas);
+    let albuns = document.getElementById("albuns")
+    let album = document.createElement("h2")
+    let generos = document.createElement("h2")
+    generos.innerText = "Generos"
+    album.innerText = "Albuns"
+    albuns.appendChild(album)
+    albuns.appendChild(generos)
 
-    for (let i = 0;i < musicas.artist.albums.item.length;i++){
-        document.getElementById("albuns").value += ' ' + musicas.artist.albums.item[i].desc
+    for (let i = 0;i < musicas.artist.albums.item.length;i++){  
+        let li = document.createElement('li')
+        li.innerText = musicas.artist.albums.item[i].desc
+        albuns.appendChild(li)
+    }
+    for (let i = 0;i < musicas.artist.genre.length;i++){  
+        let li = document.createElement('li')
+        li.innerText = musicas.artist.albums.genre[i].name
+        albuns.appendChild(li)
     }
 }
